e6ea74f34ad3d4ed4b1f52c34f4bef8f9439b474
Jack Storrs <jackstorrs8@gmail.com>
2016-10-09 23:48:00 -0400

    Started with main.js, struggling with syntax and scope. Succesfully compiled prices into an array, but unable to do anything with the new array yet.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log(items.price)
+// var priceAverage = function (items) {
+//   var newPriceArray = []
+//   for (var count = 1; count < items.length; count++) {
+//     var priceArray = items[count].prices.map();
+//     newPriceArray.push(priceArray)
+//   };
+// };
+
+var priceAverage = function (items) {
+  for (var i = 0; i < items.length; i++) {
+    var priceArray = items[i].price;
+    return priceArray.length;
+  };
+};
